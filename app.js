0b813861ec406524ade3aefdabf91a3e2613ecdd
Tesfaye Aweke <tesfayee57@gmail.com>
2024-10-07 18:30:38 +0300

    a reset button is added to the grid

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const appendDiv = () => {
     const paragraph = document.createElement('p');
 
 
-    paragraph.textContent = 'hello';
+    paragraph.textContent = 'colors';
 
     squareDiv.appendChild(paragraph);
     container.appendChild(squareDiv);
@@ -21,28 +21,27 @@ const numberOfAppends = (num) => {
 
 }
 
-const divs = document.querySelectorAll('div');
-const numberOfRows = parseInt(prompt('Enter the number of row and columns:'));
 
-const numberOfElements = numberOfRows * numberOfRows;
-const widthOfDiv = 100 / numberOfRows;
 
-//a functionlity to make the default amount even row and columns by taking the number of rows needed as a parameter. 
-const makeDefaultRows = (rows) => {
-    
+//a functionlity to make the default amount of even row and columns by taking the number of rows needed as a parameter. 
+const makeRows = (rows) => {
+
     const widthOfDiv = 100 / rows;
     const divs = document.querySelectorAll('div');
     divs.forEach((div) => {
-        div.style.flexBasis = `${widthOfDiv}%`
+        div.style.flexBasis = `${widthOfDiv}%`;
+        div.style.height = `${widthOfDiv}%`;
+        div.style.flexGrow = `0`;
+
     })
 
 
 }
 //256 div elements made to cover for the whole 16*16 need of the element. 
 const defaultElementsNumber = 256;
-const defaultnumberOfRows= Math.sqrt(defaultElementsNumber);
+const defaultnumberOfRows = Math.sqrt(defaultElementsNumber);
 numberOfAppends(defaultElementsNumber);
-makeDefaultRows(defaultnumberOfRows);
+makeRows(defaultnumberOfRows);
 
 
 
@@ -78,7 +77,63 @@ const container = document.querySelector('.container');
 
 // })
 
+//addEventListener is added to the parent container of the div to change the background color.
 container.addEventListener('mouseover', (e) => {
     e.stopPropagation();
     e.target.style.backgroundColor = changeColor();
 })
+
+const gridButton = document.querySelector('.grid-control #grid-button');
+const resetButton = document.querySelector('.grid-control #reset-button')
+const divs = document.querySelectorAll('.container div');
+let isReset = false;
+
+gridButton.addEventListener('click', () => {
+    
+    // const divs = document.querySelectorAll('div');
+    numberOfAppends(defaultElementsNumber);
+    makeRows(defaultnumberOfRows);
+    const numberOfRows = parseInt(prompt('Enter the number of row and columns:'));
+    const numberOfElements = numberOfRows * numberOfRows;
+
+
+    // const widthOfDiv = 100 / numberOfRows;
+    divs.forEach((div) => {
+        div.remove();
+    })
+    numberOfAppends(numberOfElements);
+    makeRows(numberOfRows)
+    // console.log(numberOfRows);
+    // console.log(numberOfElements);
+    
+
+
+
+})
+const paragraphs = document.querySelectorAll('.container div p');
+
+
+resetButton.addEventListener('click', () => {
+    if (!isReset) {
+        divs.forEach((div) => {
+            div.remove()
+        })
+        const defaultElementsNumber = 256;
+        const defaultnumberOfRows = Math.sqrt(defaultElementsNumber);
+        numberOfAppends(defaultElementsNumber);
+        makeRows(defaultnumberOfRows);
+        divs.forEach((div) => {
+            div.style.backgroundColor = 'white';
+        })
+        container.style.backgroundColor = 'white';
+        paragraphs.forEach((p) => {
+            p.style.backgroundColor = 'white';
+        })
+    }
+    
+
+
+
+})
+
+
